b341debc196341ff19c4433bc2d008ddc19f1fb8
Ruslan <kros8925@gmail.com>
2020-09-03 08:26:27 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,36 +1,34 @@
-#!/usr/bin/env bash
-
-# Am I root?
-[ "$EUID" -ne 0 ] && echo "You have to run this script as the root user" && exit 1
+#!/usr/bin/env dash
 
 # Output brief information before running the script
 echo "/////////////////////////////////////////////////////////////////////////////"
 echo "//////////////////////// Created by Witty Judge /////////////////////////////"
 echo "/////////////// https://github.com/WIttyJudge/setup-linux ///////////////////"
-echo "//////////////////////// Setup linux for ${type} ////////////////////////////"
+echo "//////////////////////// Setup linux ////////////////////////////"
 echo "/////////////////////////////////////////////////////////////////////////////"
 read -n 1 -r -s -p $'/////////////////////// Press any key to continue ///////////////////////////\n\n'
 
 # List of directory names with dotfiles we need to setup
-List=(
+folders=(
 	bash
 	git
+	i3
 	neofetch
+	polybar
 	profile
 	terminator
 	zsh 
-);
+)
 
 # GNU Stow is a symlink farm manager. https://www.gnu.org/software/stow
 stowit() {
-	folder=$1
-
+	name=$1
 	# -v - see what is going on.
 	# -R - purge old links.
 	# -t - target.
-	stow -v -R -t ~ ${folder}
+	stow -v -R -t ~ $name
 }
 
-for i in ${List[@]}; do
-	stowit ${i}
+for i in ${folders[@]}; do
+	stowit $i
 done
