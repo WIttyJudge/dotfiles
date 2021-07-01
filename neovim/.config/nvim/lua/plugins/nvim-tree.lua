fe76f289e5116c0ffc39f0ac1f3745bc2dbe969d
Ruslan <kros8925@gmail.com>
2021-07-01 11:22:06 +0300

    Autocommit

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -13,42 +13,40 @@ vim.g.nvim_tree_icons = {
 }
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-  vim.g.nvim_tree_bindings = {
-    -- ["<CR>"] = ":YourVimFunction()<cr>",
-    -- ["u"] = ":lua require'some_module'.some_function()<cr>",
 
-    -- custom mappings
-    ["l"]              = tree_cb("edit"),
-    ["l"]              = tree_cb("edit"),
-    ["s"]              = tree_cb("vsplit"),
-    ["b"]              = tree_cb("split"),
-    ["t"]              = tree_cb("tabnew"),
-    ["dd"]             = tree_cb("remove"),
+vim.g.nvim_tree_bindings = {
+  -- custom mappings
+  { key = {"<CR>", "l", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+  { key = "s",                            cb = tree_cb("vsplit") },
+  { key = "b",                            cb = tree_cb("split") },
+  { key = "t",                            cb = tree_cb("tabnew") },
+  { key = "dd",                            cb = tree_cb("remove") },
 
-    -- default mappings
-    ["<2-LeftMouse>"]  = tree_cb("edit"),
-    ["<2-RightMouse>"] = tree_cb("cd"),
-    ["<C-]>"]          = tree_cb("cd"),
-    ["<"]              = tree_cb("prev_sibling"),
-    [">"]              = tree_cb("next_sibling"),
-    ["<BS>"]           = tree_cb("close_node"),
-    ["<S-CR>"]         = tree_cb("close_node"),
-    ["<Tab>"]          = tree_cb("preview"),
-    ["I"]              = tree_cb("toggle_ignored"),
-    ["H"]              = tree_cb("toggle_dotfiles"),
-    ["R"]              = tree_cb("refresh"),
-    ["a"]              = tree_cb("create"),
-    ["r"]              = tree_cb("rename"),
-    ["<C-r>"]          = tree_cb("full_rename"),
-    ["x"]              = tree_cb("cut"),
-    ["c"]              = tree_cb("copy"),
-    ["p"]              = tree_cb("paste"),
-    ["y"]              = tree_cb("copy_name"),
-    ["Y"]              = tree_cb("copy_path"),
-    ["gy"]             = tree_cb("copy_absolute_path"),
-    ["[c"]             = tree_cb("prev_git_item"),
-    ["]c"]             = tree_cb("next_git_item"),
-    ["-"]              = tree_cb("dir_up"),
-    ["q"]              = tree_cb("close"),
-
-  }
+  -- default mappings
+  { key = {"<2-RightMouse>", "<C-}>"},    cb = tree_cb("cd") },
+  { key = "<",                            cb = tree_cb("prev_sibling") },
+  { key = ">",                            cb = tree_cb("next_sibling") },
+  { key = "P",                            cb = tree_cb("parent_node") },
+  { key = "<BS>",                         cb = tree_cb("close_node") },
+  { key = "<S-CR>",                       cb = tree_cb("close_node") },
+  { key = "<Tab>",                        cb = tree_cb("preview") },
+  { key = "K",                            cb = tree_cb("first_sibling") },
+  { key = "J",                            cb = tree_cb("last_sibling") },
+  { key = "I",                            cb = tree_cb("toggle_ignored") },
+  { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+  { key = "R",                            cb = tree_cb("refresh") },
+  { key = "a",                            cb = tree_cb("create") },
+  { key = "r",                            cb = tree_cb("rename") },
+  { key = "<C->",                         cb = tree_cb("full_rename") },
+  { key = "x",                            cb = tree_cb("cut") },
+  { key = "c",                            cb = tree_cb("copy") },
+  { key = "p",                            cb = tree_cb("paste") },
+  { key = "y",                            cb = tree_cb("copy_name") },
+  { key = "Y",                            cb = tree_cb("copy_path") },
+  { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+  { key = "[c",                           cb = tree_cb("prev_git_item") },
+  { key = "}c",                           cb = tree_cb("next_git_item") },
+  { key = "-",                            cb = tree_cb("dir_up") },
+  { key = "q",                            cb = tree_cb("close") },
+  { key = "g?",                           cb = tree_cb("toggle_help") },
+}
