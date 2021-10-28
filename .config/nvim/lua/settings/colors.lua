ddebcf78b341b23429f413bc0f1c46cc2133c2d7
Ruslan <kros8925@gmail.com>
2021-10-28 17:11:15 +0300

    BashRun command for sh filetype

--- .config/nvim/lua/settings/colors.lua
+++ .config/nvim/lua/settings/colors.lua
@@ -4,9 +4,16 @@ if vim.fn.has("termguicolors") == 1 then
   vim.opt.termguicolors = true
 end
 
-vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_enable_italic_comments = true
 vim.g.gruvbox_material_sign_column_background = 'none'
-vim.cmd [[color gruvbox-material]]
+vim.g.gruvbox_material_diagnostic_text_highlight = 0
+vim.g.gruvbox_material_diagnostic_virtual_text = 'grey'
+vim.opt.background = 'light'
+
+-- require('wittyjudge/gruvbox-material').setup()
+-- require('gruvbox-material').setup()
+
+vim.cmd('colorscheme gruvbox-material')
 
 -- hi! Normal ctermbg=NONE
 -- hi! NonText ctermbg=NONE
