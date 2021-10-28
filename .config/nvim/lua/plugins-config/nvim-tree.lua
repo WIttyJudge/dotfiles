3b3df31373b3d507d5cd9e4245b7c6c15d453fc0
Ruslan <kros8925@gmail.com>
2021-10-28 16:11:13 +0300

    Change auto_close to true in nvim-tree neovim plugin.

--- .config/nvim/lua/plugins-config/nvim-tree.lua
+++ .config/nvim/lua/plugins-config/nvim-tree.lua
@@ -67,7 +67,7 @@ local config = {
   -- will not open on setup if the filetype is in this list
   ignore_ft_on_setup = { 'startify', 'dashboard' },
   -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close = false,
+  auto_close = true,
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab = false,
   -- hijacks new directory buffers when they are opened.
