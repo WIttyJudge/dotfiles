4e2bb5d28f98e9d29bdc3223736aaa12e35935ba
Ruslan <kros8925@gmail.com>
2021-11-10 15:46:26 +0300

    Refactoring.

--- .config/nvim/lua/settings/autocmds.lua
+++ .config/nvim/lua/settings/autocmds.lua
@@ -3,59 +3,60 @@ local cmd = vim.cmd
 -- Highlight like an error if more then 80 lines of code in one line.
 -- match ErrorMsg '\%>80v.\+'
 
--- Highlight yanked text
-cmd([[
-autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-]])
-
 -- Restore cursor to where it was when the file was closed
 cmd([[
 autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
 ]])
 
--- remove  trailing whitespace on save
+-- remove trailing whitespace on save
 -- autocmd BufWritePre * %s/\s\+$//e
-
--- Autocompile suckless tools
--- cmd([[
--- augroup SucklessTools
---   autocmd!
---   
---   autocmd BufWritePost ~/suckless-tools/dwmblocks/config.h
---         \ !cd ~/suckless-tools/dwmblocks/;
---         \ sudo make install && { killall -q dwmblocks;setsid -f dwmblocks }
--- 
---   autocmd BufWritePost ~/suckless-tools/dwm/config.h
---         \ !cd ~/suckless-tools/dwm/;
---         \ sudo make install
--- 
---   autocmd BufWritePost ~/suckless-tools/dmenu/config.h
---         \ !cd ~/suckless-tools/dmenu/;
---         \ sudo make install
--- augroup END
--- ]])
 
 -- execute goimports linter
 -- autocmd BufWritePre *.go :silent! lua require('go.format').gofmt()
 -- autocmd BufWritePre *.go :silent! lua require('custom.go.format').goimports(1000)
-
-cmd([[
-autocmd BufWritePre *.go :silent! :Goimport
-]])
-
-cmd([[
-autocmd FileType fugitive map <buffer> q gq<CR>
-]])
-
--- Add command to execute current file.
-cmd([[
-autocmd FileType go command! GoRun :call mappings#RunWith("go run")
-autocmd FileType sh command! BashRun :call mappings#RunWith("bash")
-
-autocmd FileType javascript command! JsRun :call mappings#RunWith("node")
-autocmd FileType typescript command! TsRun :call mappings#RunWith("npx ts-node")
-]])
-
 -- autocmd BufWritePre *.go :silent! :Gofmt
-
 -- autocmd BufWritePre (InsertLeave?) *.go <buffer> :silent! lua vim.lsp.buf.formatting_sync(nil,500)
+
+local autogroups_list = {
+  _general_settings = {
+    -- Highlight yanked text
+    { "TextYankPost", "*", "silent! lua vim.highlight.on_yank()" },
+  },
+  _filetype = {
+    { "FileType", "fugitive", "map <buffer> q gq<CR>" },
+    { "BufWritePre", "*.go", ":silent! :Goimport" }
+  },
+  _autocompile = {
+    -- Compile suckless-tools on save
+    {
+      "BufWritePost", "~/suckless-tools/dwmblocks/config.h",
+      "!cd ~/suckless-tools/dwmblocks;",
+      "sudo make clean install && { killall -q dwmblocks;setsid -f dwmblocks }"
+    },  
+    {
+      "BufWritePost", "~/suckless-tools/dwm/config.h",
+      "!cd ~/suckless-tools/dwm; sudo make clean install"
+    }, 
+    {
+      "BufWritePost", '~/suckless-tools/dmenu/config.h',
+      "!cd ~/suckless-tools/dmenu; sudo make clean install"
+    }
+  }
+}
+
+-- Create autocommand groups based on the passed definitions
+local function define_augroups(definitions)
+  for group_name, definition in pairs(definitions) do
+    vim.cmd("augroup " .. group_name)
+    vim.cmd "autocmd!"
+
+    for _, def in pairs(definition) do
+      local command = table.concat(vim.tbl_flatten { "autocmd", def }, " ")
+      vim.cmd(command)
+    end
+
+    vim.cmd "augroup END"
+  end
+end
+
+define_augroups(autogroups_list)
--- .config/nvim/lua/settings/commands.lua
+++ .config/nvim/lua/settings/commands.lua
@@ -1,15 +1,28 @@
--- Useful
-vim.cmd([[
-command! LspFormat execute 'lua vim.lsp.buf.formatting_sync(nil, 1000)'
-command! Filetype execute 'lua print(vim.bo.filetype)'
-]])
-
 -- execute goimports linter
-vim.cmd([[
-command! Goimports :lua require('custom.go.format').goimports(1000)
-]])
+-- vim.cmd([[ command! Goimports :lua require('custom.go.format').goimports(1000) ]])
 
--- Git
-vim.cmd([[
-command! GitBlameByLineToggle :Gitsigns toggle_current_line_blame
-]])
+local commands_list = {
+  -- General
+  [[ command! Filetype execute 'lua print(vim.bo.filetype)' ]], 
+
+  -- Git
+  [[ command! GitBlameByLineToggle :Gitsigns toggle_current_line_blame ]],
+
+  -- Execute files
+  [[ "FileType", "go", "command! GoRun :call mappings#RunWith('go run')" ]],
+  [[ "FileType", "sh", "command! BashRun :call mappings#RunWith('bash')" ]],
+  [[ "FileType", "javascript", "command! JsRun :call mappings#RunWith('node')" ]],
+  [[
+    "FileType", "typescript",
+    "command! TsRun :call mappings#RunWith('npx ts-node')"
+  ]], -- Lsp
+  [[ command! LspFormat execute 'lua vim.lsp.buf.formatting_sync(nil, 1000)' ]]
+}
+
+local function define_commands(commands)
+  for _, command in pairs(commands) do
+    vim.cmd(command)
+  end
+end
+
+define_commands(commands_list)
