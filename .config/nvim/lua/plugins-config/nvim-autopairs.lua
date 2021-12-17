5a60febb93a06aea4b5f4604bbd4608c8be3680b
Ruslan <kros8925@gmail.com>
2021-12-17 11:36:46 +0000

    Refactor nvim-autopairs

--- .config/nvim/lua/plugins-config/nvim-autopairs.lua
+++ .config/nvim/lua/plugins-config/nvim-autopairs.lua
@@ -1,10 +1,10 @@
 -- https://github.com/windwp/nvim-autopairs
 
 require('nvim-autopairs').setup({
-  check_ts = true
+  disable_filetype = { 'TelescopePrompt', 'vim' }
+  -- check_ts = true,
 })
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = ' ' } }))
-
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
