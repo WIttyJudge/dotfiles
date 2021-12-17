492a6b2d5e9df0564ea065c3774be2a478a955cc
Ruslan <kros8925@gmail.com>
2021-12-17 11:31:49 +0000

    Minor refactor options, delete useless.

--- .config/nvim/lua/settings/options.lua
+++ .config/nvim/lua/settings/options.lua
@@ -1,13 +1,23 @@
 -- Read more:
 -- https://github.com/nanotee/nvim-lua-guide#using-meta-accessors
 
+-- Enables syntax highlighing
+vim.cmd([[ filetype plugin on ]])
+
+vim.opt.shortmess:append('c')
+vim.o.sessionoptions = 'buffers,curdir,folds,tabpages,winsize'
+
+-- Stop newline continution of comments
+vim.opt.formatoptions:remove('c', 'r', 'o', 'l')
+vim.opt.formatoptions:append('t')
+
 local default_options = {
-  -- DEFAULTS
+  -- defaults
   encoding = 'UTF-8', -- Always use UTF-8
-  fileencoding = 'UTF-8',
+  syntax = 'enable',
   clipboard = 'unnamedplus', -- Copy paste between vim and everything else
 
-  -- INDENT OPTIONS
+  -- indent options
   tabstop = 2,
   shiftwidth = 2,
   expandtab = true,
@@ -15,40 +25,53 @@ local default_options = {
   smartindent = true,
   autoindent = true,
 
-  -- SEARCH OPTIONS
+  -- search options
   ignorecase = true, -- Case insensitive searching
   smartcase = true, -- Case-sensitive if expresson contains a capital letter
   hlsearch = true, -- Highlight search results
 
-  -- COMPLETION OPTIONS
+  -- perfomance
   updatetime = 200,
+  timeoutlen = 400,
+  redrawtime = 1500,
+  ttimeoutlen = 10,
+
+  -- completion options
   completeopt = 'menuone,noinsert,noselect',
   -- Remove `Pattern not found` nvim-compe
   pumheight = 10, -- Make pop up menu smaller
 
-  -- LINE OPTIONS
-  scrolloff = 7,
+  -- ui
   number = true,
   relativenumber = true,
-  textwidth = 120,
-  linebreak = true,
-  -- wrap = true, Set automatic wrapping to new line if characters more then 80
-  -- colorcolumn = "80",
-  signcolumn = 'yes',
   cursorline = true, -- Enable highlighting on the current_line
-
-  -- STATUSLINE, TABLINE, MESSAGES
+  signcolumn = 'yes',
   laststatus = 2, -- Always display the status line
-  showtabline = 2,  -- Always show tables
-  cmdheight = 2, -- More space for displaying messages
-
+  -- wrap = true, Set automatic wrapping to new line if characters more then 80
+  scrolloff = 7,
+  sidescrolloff = 3, -- Lines to scroll horizontally
+  showmode = false, -- We don't need to see things like -- INSERT -- anymore
   mouse = 'a', -- Enable mouse
-
-  -- For better experience this session manager
-  -- sessionoptions='blank,buffers,curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal'
-
   splitbelow = true, -- Horizontal splits will open below
   splitright = true, -- Vertical splits will open  right
+  colorcolumn = "79",
+  -- textwidth = 120,
+  -- linebreak = true,
+
+  -- something new
+  list = true,
+  listchars = {
+    tab = '❘-',
+    trail = '·',
+    lead = '·',
+    extends = '»',
+    precedes = '«',
+    nbsp = '×',
+  },
+
+  -- statusline, tabline, messages
+  showtabline = 2,  -- Always show tables
+  cmdheight = 2, -- More space for displaying messages
 
   -- Enable the integrated undo features.
   undofile = true,
@@ -60,27 +83,14 @@ local default_options = {
   backup = false,
   writebackup = false,
 
-  showmode = false, -- We don't need to see things like -- INSERT -- anymore
-
   -- Buffer/Tabs/Windows
-  hidden = true,
+  -- hidden = true,
 
-  fixeol = true,
+  -- fixeol = true,
 
   -- Auto-reloading a file as it changed on disk
-  autoread = true
+  -- autoread = true
 }
-
--- Enables syntax highlighing
-vim.cmd([[ syntax enable ]])
-vim.cmd([[ filetype plugin on ]])
-
-vim.opt.shortmess:append('c')
-vim.o.sessionoptions = 'buffers,curdir,folds,tabpages,winsize'
-
--- Stop newline continution of comments
-vim.opt.formatoptions:remove('c', 'r', 'o', 'l')
-vim.opt.formatoptions:append('t')
 
 for k, v in pairs(default_options) do
   vim.opt[k] = v
