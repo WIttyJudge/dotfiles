e8202389b3d8083a0d92d4978acfd9252b2a6380
Ruslan <kros8925@gmail.com>
2022-07-09 14:46:58 +0000

    Update

--- .config/nvim/lua/plugins/configs/true-zen.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/Pocco81/TrueZen.nvim
-local present, true_zen = pcall(require, "true-zen")
-
-if not present then
-  return
-end
-
-true_zen.setup()
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,23 +2,23 @@ local present, packer = pcall(require, 'packer')
 
 -- Install packer  if it is not already installed.
 if not present then
-  local packer_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
   print('Cloning packer..')
   -- remove the dir before cloning
-  vim.fn.delete(packer_path, 'rf')
-  vim.fn.system({
+  fn.delete(packer_path, 'rf')
+
+  fn.system({
     'git',
     'clone',
     '--depth',
     '1',
     'https://github.com/wbthomason/packer.nvim',
-    packer_path,
+    install_path,
   })
 
   vim.cmd('packadd packer.nvim')
-  require "plugins"
-  vim.cmd("PackerSync")
 
   present, packer = pcall(require, 'packer')
 
