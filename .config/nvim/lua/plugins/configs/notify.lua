597691f738effa8814347c05f7dad4f99263973c
Ruslan <kros8925@gmail.com>
2022-08-16 12:40:45 +0000

    Change notify delay

--- .config/nvim/lua/plugins/configs/notify.lua
+++ .config/nvim/lua/plugins/configs/notify.lua
@@ -21,7 +21,7 @@ local config = {
   render = "default",
 
   -- Default timeout for notifications
-  timeout = 5000,
+  timeout = 2000,
 
   -- For stages that change opacity this is treated as the highlight behind the window
   -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
