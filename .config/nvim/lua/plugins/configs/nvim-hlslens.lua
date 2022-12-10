11ff193753e7819145da43bf0b301bf3d56844f4
Ruslan <kros8925@gmail.com>
2022-12-10 16:50:05 +0400

    Change nvim-hlslens default settings.

--- .config/nvim/lua/plugins/configs/nvim-hlslens.lua
+++ .config/nvim/lua/plugins/configs/nvim-hlslens.lua
@@ -7,32 +7,34 @@ end
 
 local config = {
   calm_down = true,
-  override_lens = function(render, plist, nearest, idx, r_idx)
+  nearest_only = true,
+  nearest_float_when = 'always',
+  override_lens = function(render, posList, nearest, idx, relIdx)
     local sfw = vim.v.searchforward == 1
     local indicator, text, chunks
-    local abs_r_idx = math.abs(r_idx)
-    if abs_r_idx > 1 then
-      indicator = string.format("%d%s", abs_r_idx, sfw ~= (r_idx > 1) and "" or "")
-    elseif abs_r_idx == 1 then
-      indicator = sfw ~= (r_idx == 1) and "" or ""
+    local absRelIdx = math.abs(relIdx)
+    if absRelIdx > 1 then
+      indicator = ("%d%s"):format(absRelIdx, sfw ~= (relIdx > 1) and "▲" or "▼")
+    elseif absRelIdx == 1 then
+      indicator = sfw ~= (relIdx == 1) and "▲" or "▼"
     else
       indicator = ""
     end
 
-    local lnum, col = unpack(plist[idx])
+    local lnum, col = unpack(posList[idx])
     if nearest then
-      local cnt = #plist
+      local cnt = #posList
       if indicator ~= "" then
-        text = string.format("[%s %d/%d]", indicator, idx, cnt)
+        text = ("[%s %d/%d]"):format(indicator, idx, cnt)
       else
-        text = string.format("[%d/%d]", idx, cnt)
+        text = ("[%d/%d]"):format(idx, cnt)
       end
       chunks = { { " ", "Ignore" }, { text, "HlSearchLensNear" } }
     else
-      text = string.format("[%s %d]", indicator, idx)
+      text = ("[%s %d]"):format(indicator, idx)
       chunks = { { " ", "Ignore" }, { text, "HlSearchLens" } }
     end
-    render.set_virt(0, lnum - 1, col - 1, chunks, nearest)
+    render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
   end,
 }
 
