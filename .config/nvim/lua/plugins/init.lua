3d29d5cc512ef5783e21431b137ce791ae66ec0a
Ruslan <kros8925@gmail.com>
2023-05-10 12:47:27 +0300

    Add new script

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -261,13 +261,17 @@ return {
   },
 
   -- Golang
+
   {
     "ray-x/go.nvim",
-    ft = { "go", "gomod" },
-    dependencies = {
+    dependencies = {  -- optional packages
       "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
     },
     config = true,
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
 
   -- Markdown
