a6076a97ff3b92ed61fd816d8abf69b4df6df645
Ruslan <kros8925@gmail.com>
2025-05-10 16:24:01 +0300

    fix mason

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -17,8 +17,9 @@ return {
 
   {
     "williamboman/mason.nvim",
+    version = "1.11.0",
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
+      { "williamboman/mason-lspconfig.nvim", version = "1.32.0" }
     },
     build = ":MasonUpdate",
   },
