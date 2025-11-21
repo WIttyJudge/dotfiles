35f206ad067a66265fc8282d0caf7924aa83fae1
Ruslan <kros8925@gmail.com>
2025-11-21 18:10:35 +0200

    delete nvim-cmp

--- .config/nvim/lua/lsp/capabilities.lua
+++ .config/nvim/lua/lsp/capabilities.lua
@@ -1,4 +1,3 @@
-local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 local has_blink, blink = pcall(require, "blink.cmp")
 
 local globalCapabilities = {
@@ -14,7 +13,6 @@ local capabilities = vim.tbl_deep_extend(
   "force",
   {},
   vim.lsp.protocol.make_client_capabilities(),
-  has_cmp and cmp_nvim_lsp.default_capabilities() or {},
   has_blink and blink.get_lsp_capabilities() or {},
   globalCapabilities or {}
 )
