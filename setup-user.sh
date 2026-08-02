42081905aecef55b142727e0675f6c21c25a6bb0
Ruslan <kros8925@gmail.com>
2026-08-02 22:25:11 +0300

    feat(setup-users): added macos-related setup

--- setup-user.sh
+++ setup-user.sh
@@ -36,34 +36,48 @@ link_all_files_in_folder() {
   find $orig_dir -type f -exec ln -sfv "{}" $dest_dir \;
 }
 
+is_macos() {
+  [ "$(uname -s)" = "Darwin" ]
+}
+
 echo "==========================="
 echo "Setting up user dotfiles..."
 echo "==========================="
 
+# Configs shared between Linux and macOS
 link ".bashrc"
 link ".profile"
-link ".xinitrc"
-link ".xprofile"
-link ".Xresources"
 link ".zprofile"
 
 link ".config/alacritty"
-link ".config/dunst"
-link ".config/foot"
 link ".config/git"
-link ".config/gtk-3.0"
 link ".config/htop"
-link ".config/hypr"
 link ".config/lazygit"
 link ".config/nvim"
+link ".config/shell"
+link ".config/tmux"
+link ".config/zsh"
+
+link_all_files_in_folder ".local/bin"
+
+if is_macos; then
+  echo "Detected macOS, skipping Linux-only (X11/Wayland/XDG) configs."
+  exit 0
+fi
+
+link ".xinitrc"
+link ".xprofile"
+link ".Xresources"
+
+link ".config/dunst"
+link ".config/foot"
+link ".config/gtk-3.0"
+link ".config/hypr"
 link ".config/pcmanfm"
 link ".config/rofi"
-link ".config/shell"
 link ".config/swappy"
-link ".config/tmux"
 link ".config/waybar"
 link ".config/zathura"
-link ".config/zsh"
 link ".config/mimeapps.list"
 link ".config/pavucontrol.ini"
 link ".config/user-dirs.dirs"
@@ -78,4 +92,3 @@ link ".config/user-dirs.dirs"
 link ".local/share/bg"
 link_all_files_in_folder ".local/share/applications"
 link_all_files_in_folder ".local/share/icons/fontawesome"
-link_all_files_in_folder ".local/bin"
