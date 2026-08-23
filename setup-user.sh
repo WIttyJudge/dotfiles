275b35796a647e22d020f0067ecf1eb7a6133b76
Ruslan <kros8925@gmail.com>
2026-08-23 17:48:51 +0300

    updated karabiner

--- setup-user.sh
+++ setup-user.sh
@@ -95,7 +95,7 @@ link ".config/zsh"
 if is_macos; then
   link ".config/aerospace"
   link ".config/borders"
-  link ".config/karabiner/karabiner.json"
+  link ".config/karabiner"
   link_files_in_folder ".local/bin"
   link_files_in_folder ".local/bin/tmux" ".local/bin"
 
